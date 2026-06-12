2a1bad7088b92688eb08a7b2ad2d0d2f460aee3e
valerie-ekeigwe <valerie.ekeigwe@gmail.com>
2026-06-12 13:06:57 -0400

    Allow all origins for CORS

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ app = FastAPI(title="ASHA Backend")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
